a90587b1f8bfb366fbde41d6d803d41f489a2987
sali <saadali93@gmail.com>
2021-03-02 14:29:55 -0600

    added user and global contexts

---
+++ src/context/GlobalContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useEffect } from "react";
+
+const initialState = {
+  setDarkMode: (darkMode_: boolean) => {},
+  darkMode: false,
+}
+
+export const GlobalContext = React.createContext(initialState);
+
+export const GlobalContextProvider: React.FC = ({children}) => {
+  const [darkMode, setDarkMode] = useState(false);
+  // TODO add logic to change color scheme
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        setDarkMode: (toDark_: boolean) => {
+          localStorage.setItem('dark_mode', String(toDark_));
+          setDarkMode(toDark_);
+        },
+        darkMode,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+}
+
+export default GlobalContextProvider;
---
+++ src/context/UserContext.tsx
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useEffect } from "react";
+
+const initialState = {
+  positions: [],
+}
+
+export const UserContext = React.createContext(initialState);
+
+export const UserContextProvider: React.FC = ({children}) => {
+  const [positions, setPositions] = useState([]);
+
+  // TODO pull user positions for all synths
+  const updateUserPositions = (positions_: Array<object>) => {
+  }
+
+  return (
+    <UserContext.Provider
+      value={{
+        positions,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export default UserContextProvider;
